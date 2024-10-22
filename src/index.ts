3fd968a570ca4a7be23c14d9d6a890e1c2a29cae
RichardGarciaP <richardgarcia2499@gmail.com>
2024-10-21 22:26:58 -0500

    feat: observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+import { Observable, Observer } from "rxjs";
 
+const observer: Observer<any> = {
+  next: (value) => console.log(`[Next]: ${value}`),
+  error: (error) => console.warn(`[Error]: ${error}`),
+  complete: () => console.info("[Completed]"),
+};
 
-console.log('Hola Mundo!');
+// const observable$ = Observable.create()
+const observable$ = new Observable<string>((subscriber) => {
+  subscriber.next("Hola");
+  subscriber.next("Mundo");
 
+  subscriber.next("Hola");
+  subscriber.next("Mundo");
 
+  //   const a = undefined;
+  //   a.nombre = "Ragnar";
 
+  subscriber.complete();
 
+  subscriber.next("Hola");
+  subscriber.next("Mundo");
+});
 
+// observable$.subscribe(
+//   (valor) => console.log("next: ", valor),
+//   (error) => console.warn("error: ", error),
+//   () => console.info("Completed")
+// );
 
-
+observable$.subscribe(observer);
